14e09cb097f4580b1540fbd41119042cc92e4af1
tafhaque <tafhaque01@gmail.com>
2019-12-21 15:55:28 +0000

    Arrays
    Worked on arrays

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,10 +1,49 @@
 const notes = [{
-
+    title: 'My next trip',
+    body: 'I would like to go to Morocco'
 }, {
-
+    title: 'Habits to work on',
+    body: 'Exercise. Eating a bit better.'
 }, {
-
+    title: 'Office modification',
+    body: 'Get a new seat'
 }]
+
+
+// Using .find()
+const findNote = function(notes, noteTitle) {
+    return notes.find(function(note, index) {
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+}
+
+
+// Using .findIndex()
+// const findNote = function(notes, noteTitle) {
+//     const index = notes.findIndex(function(note, index) {
+//         return note.title.toLowerCase() === noteTitle.toLowerCase()
+//     })
+//     return notes[index]
+// }
+
+const note = findNote(notes, 'Some other office modification')
+console.log(note);
+
+
+// console.log(notes.length);
+// console.log(notes)
+
+// Looks for the first index match
+// const index = notes.findIndex(function(note, index) {
+//     console.log(note);
+//     return note.title === 'Habits to work on'
+// });
+
+// console.log(index);
+
+
+// Array of Strings
+//const notes = ['Note 1', 'Note 2', 'NOte 3']
 
 // console.log(notes.pop())
 // notes.push('My new note')
@@ -14,24 +53,26 @@ const notes = [{
 
 // notes.splice(1, 1, 'This is the new second item')
 
-notes[2] = 'This is the new note 3'
+// notes[2] = 'This is the new note 3'
 
-notes.forEach(function(item, index) {
-    console.log(index);
-    console.log(item);
-})
+// notes.forEach(function(item, index) {
+//     console.log(index);
+//     console.log(item);
+// })
 
-console.log(notes.length);
-console.log(notes)
+// let someObject = {};
+// let otherObject = someObject;
+// console.log(someObject === otherObject);
 
-console.log(notes.indexOf('note 2'))
+// console.log(notes.indexOf({}));
 
-// Counting.. 1
 
-for (let count = 0; count <= 2; count++) {
-    console.log(count);
 
-}
+// If output is -1 from indexOf, then item does not exist in the array. 
+// If output is greater than -1 including 0, then the item exists somewhere in the array  
+// console.log(notes.indexOf('Note 2'))
+
+
 
 // Counting... 1
 // for (let count = 0; count <= 2; count++) {
@@ -46,4 +87,4 @@ for (let count = 0; count <= 2; count++) {
 // for (let count = notes.length - 1; count >= 0; count--) {
 //     console.log(notes[count])
 
-// }
+//
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,24 +1,74 @@
-const toDos = ['Finish js bootcamp', 'Find a software job', 'Move to London', 'Live life', 'Try new things']
+// Array of strings
+//const todos = ['Finish js bootcamp', 'Find a software job', 'Move to London', 'Live life', 'Try new things']
 
-// console.log(toDos.splice(2, 1))
-// toDos.push('Exercise')
-// console.log(toDos.shift())
+// 1. Convert array of strings to array of objects -> properties are - text, completed (true or false)
+// 2. Create function to remove a todo by text value
+
+const todos = [{
+    text: 'Finish js bootcamp',
+    completed: false
+}, {
+    text: 'Find a software job',
+    completed: false
+}, {
+    text: 'Move to London',
+    completed: false
+}, {
+    indextext: 'Live life',
+    completed: true
+}, {
+    text: 'Try new things',
+    completed: true
+}]
 
 
-console.log(`You have ${toDos.length} todos`);
+const deleteTodo = function(todos, todoText) {
+    const index = todos.findIndex(function(todo) {
+        return todo.text.toLowerCase() === todoText.toLowerCase()
+    })
+    if (index > -1) {
+        todos.splice(index, 1)
+    }
+}
 
-toDos.forEach(function(item, index) {
-    console.log(`${index + 1}. ${item}`);
-})
+// const note = findNote(notes, 'Some other office modification')
+// console.log(note);
+deleteTodo(todos, 'live life')
+console.log(todos);
 
-// console.log(toDos);
-// console.log(`You have ` + toDo.length + ' todos');
 
-// console.log(`Todo: ${toDos[0]}`);
-// console.log(`Todo: ${toDos[toDos.length - 2]}`);
+
+
+// todos.splice(2, 1);
+// todos.push('Exercise');
+// todos.shift();
+
+// // console.log(todos.splice(2, 1))
+// // todos.push('Exercise')
+// // console.log(todos.shift())
+
+
+// console.log(`You have ${todos.length} todos`);
+
+// todos.forEach(function(todo, index) {
+//     console.log(`${index + 1}. ${todo}`);
+// })
+
+
+// console.log(todos);
+// console.log(`You have ` + todo.length + ' todos');
+
+// console.log(`Todo: ${todos[0]}`);
+// console.log(`Todo: ${todos[todos.length - 2]}`);
 
 // console.log('\n');
 
-// for (count = 0; count < toDos.length; count++) {
-//     console.log(`${count + 1}. ${toDos[count]}`);
+// for (count = 0; count < todos.length; count++) {
+//     console.log(`${count + 1}. ${todos[count]}`);
+// }
+
+// for (let count = 0; count < todos.length; count++) {
+//     const num = count + 1;
+//     const todo = todos[count];
+//     console.log(`${num}. ${todo}`);
 // }
